e08586ee592ea70599f595df558e44601583088b
unknown <xsavov@ad.fi.muni.cz>
2024-04-17 14:48:25 +0200

    fix some warnings

--- buggy_python_code_solution.py
+++ buggy_python_code_solution.py
@@ -14,7 +14,7 @@ def index():
 
         
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
-class Person(object):
+class Person():
     def __init__(self, name):
         self.name = name
 
@@ -39,7 +39,7 @@ def load_yaml(filename):
     stream = open(filename)
     deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
-    
+
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
